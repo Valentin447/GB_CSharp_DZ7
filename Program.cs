5fd8fc7e8eb8cccbc108065c5678268aef356903
Valentin447 <valentin@myttk.ru>
2022-10-07 23:03:50 +0700

    Add solution Task52

--- Program.cs
+++ Program.cs
@@ -40,6 +40,24 @@ void Task50()
 void Task52() 
 {
     // Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+    Random random = new Random();
+    int rows = random.Next(3, 6);
+    int columns = random.Next(3, 6);
+    int[,] numbers = new int[rows, columns];
+    Console.WriteLine($"Размер массива: {rows} x {columns}");
+    FillArreyInt(numbers, 0, 99);
+    ShowArreyInt(numbers);
+
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            sum += numbers[i, j];
+        }
+        Console.WriteLine($"Среднее арифметическое {j + 1}-ого столбца равно: {Math.Round(Convert.ToDouble(sum) / Convert.ToDouble(rows), 2)}"); 
+    }
 }
 void ShowArreyDouble(double[,] arr)
 {
@@ -63,6 +81,29 @@ void FillArreyDouble(double[,] arr, double min = 0, double max = 0, int round = 
         }
     }
 }
+void ShowArreyInt(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArreyInt(int[,] arr, int min = 0, int max = 0)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(min, max + 1);
+        }
+    }
+}
+
 //Task47();
-Task50();
-//Task52();
+//Task50();
+Task52();
